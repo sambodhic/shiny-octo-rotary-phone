fb014e3bb2242e1dd66b9d3c66877596346b7418
Sambodhi <sambodhi.chatterjee@gmail.com>
2023-08-08 09:46:14 -0700

    add db settings

---
+++ allinone/postgresql-fs-db.tf
@@ -0,0 +1,6 @@
+resource "azurerm_postgresql_flexible_server_database" "database" {
+  name      = "${var.database_name_prefix}-db"
+  server_id = azurerm_postgresql_flexible_server.database.id
+  collation = "en_US.UTF8"
+  charset   = "UTF8"
+}
