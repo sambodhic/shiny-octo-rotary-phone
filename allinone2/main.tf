b290578c8955d5e853973b35b1e29b317fb4352d
Sambodhi <sambodhi.chatterjee@gmail.com>
2023-08-09 02:54:20 -0700

    adding key vault

--- allinone2/main.tf
+++ allinone2/main.tf
@@ -219,6 +219,63 @@ resource "azurerm_batch_application" "kai" {
   account_name        = azurerm_batch_account.kai.name
 }
 
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/key_vault
+data "azurerm_client_config" "current" {}
+
+resource "random_string" "azurerm_key_vault_name" {
+  length  = 13
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
+
+locals {
+  current_user_id = coalesce(var.msi_id, data.azurerm_client_config.current.object_id)
+}
+
+resource "azurerm_key_vault" "vault" {
+  name                       = coalesce(var.vault_name, "vault-${random_string.azurerm_key_vault_name.result}")
+  location                   = azurerm_resource_group.kai.location
+  resource_group_name        = azurerm_resource_group.kai.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = var.sku_name
+  soft_delete_retention_days = 7
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = local.current_user_id
+
+    key_permissions    = var.key_permissions
+    secret_permissions = var.secret_permissions
+  }
+}
+
+resource "random_string" "azurerm_key_vault_key_name" {
+  length  = 13
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
+
+resource "azurerm_key_vault_key" "key" {
+  name = coalesce(var.key_name, "key-${random_string.azurerm_key_vault_key_name.result}")
+
+  key_vault_id = azurerm_key_vault.vault.id
+  key_type     = var.key_type
+  key_size     = var.key_size
+  key_opts     = var.key_ops
+
+  rotation_policy {
+    automatic {
+      time_before_expiry = "P30D"
+    }
+
+    expire_after         = "P90D"
+    notify_before_expiry = "P29D"
+  }
+}
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/private_endpoint
 resource "azurerm_private_endpoint" "kaisa" {
@@ -245,6 +302,20 @@ resource "azurerm_private_endpoint" "kaiba" {
     name                           = "kai-privateserviceconnection"
     private_connection_resource_id = azurerm_batch_account.kai.id
     subresource_names              = ["batchAccount"]
+    is_manual_connection           = false
+  }
+}
+
+resource "azurerm_private_endpoint" "kaikv" {
+  name                = "kai-kv-endpoint"
+  location            = azurerm_resource_group.kai.location
+  resource_group_name = azurerm_resource_group.kai.name
+  subnet_id           = azurerm_subnet.backend.id
+
+  private_service_connection {
+    name                           = "kai-privateserviceconnection"
+    private_connection_resource_id = azurerm_key_vault.vault.id
+    subresource_names              = ["vault"]
     is_manual_connection           = false
   }
 }
